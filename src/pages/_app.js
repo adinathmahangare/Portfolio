fa2b8dd7b203bf42c5126a892df31dca434c26ff
Adinath Mahangare <adinathmahangare914@gmail.com>
2024-02-07 16:23:16 +0530

    Add Navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
+import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
 import {Montserrat} from "next/font/google"
+import Head from 'next/head'
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -8,11 +10,12 @@ const montserrat = Montserrat({
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <><head>
+    <><Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-    </head>
+    </Head>
     <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen` }>
+      <NavBar />
       <Component {...pageProps} />
     </main>
     </>
