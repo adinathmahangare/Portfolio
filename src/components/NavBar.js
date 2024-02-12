3933902cef980d71e34364aba7612a3475ba25f1
Adinath Mahangare <adinathmahangare914@gmail.com>
2024-02-12 15:17:46 +0530

    Add icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Logo from './Logo'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { TwitterIcon, GithubIcon, LinkedInIcon, PinterestIcon, DribbbleIcon } from './Icons'
+import { motion } from 'framer-motion'
 
 const CustomLink = ({href, title, className=""})=>{
     const router = useRouter();
@@ -29,13 +31,32 @@ const NavBar = () => {
                 <CustomLink href="/articles" title= "Articles" className='ml-4'/>
                 
             </nav>
-            <nav>
-                <Link href="/" target={'_blank'}>T</Link>
-                <Link href="/" target={'_blank'}>T</Link>
-                <Link href="/" target={'_blank'}>T</Link>
-                <Link href="/" target={'_blank'}>T</Link>
-                <Link href="/" target={'_blank'}>T</Link>
-                <Link href="/" target={'_blank'}>T</Link>
+            <nav className='flex item-center justify-center flex-wrap'>
+                <motion.a href="https://twitter.com" target={'_blank'}
+                whileHover={{y:-2}}
+                whileTap={{scale:0.9}} 
+                className='w-6 mr-3'
+                ><TwitterIcon /></motion.a>
+                <motion.a href="https://github.com" target={'_blank'}
+                whileHover={{y:-2}} 
+                whileTap={{scale:0.9}}
+                className='w-6 mx-3'
+                ><GithubIcon /></motion.a>
+                <motion.a href="https://linkedin.com" target={'_blank'}
+                whileHover={{y:-2}} 
+                whileTap={{scale:0.9}}
+                className='w-6 mx-3'
+                ><LinkedInIcon /></motion.a>
+                <motion.a href="https://twitter.com" target={'_blank'}
+                whileHover={{y:-2}} 
+                whileTap={{scale:0.9}}
+                className='w-6 mx-3'
+                ><PinterestIcon /></motion.a>
+                <motion.a href="https://twitter.com" target={'_blank'}
+                whileHover={{y:-2}} 
+                whileTap={{scale:0.9}}
+                className='w-6 ml-3'
+                ><DribbbleIcon /></motion.a>
 
             </nav>
 
