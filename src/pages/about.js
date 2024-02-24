37dc334c4b34c150e9083af440bf384cbda3a7e0
Adinath Mahangare <adinathmahangare914@gmail.com>
2024-02-24 17:12:21 +0530

    Add Number Animation

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,33 @@ import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
-import React from 'react'
-import profilePic from "../../public/images/profile/developer-pic-2.jpg"
+import React, { useEffect, useRef } from 'react'
+import profilePic from "../../public/images/profile/profile.png"
+import { useInView, useMotionValue, useSpring } from 'framer-motion'
+
+const AnimatedNumbers = ({value}) => {
+    const ref = useRef(null);
+
+    const motionValue = useMotionValue(0);
+    const springValue = useSpring(motionValue, {duration:3000})
+    const isinView = useInView(ref, {once:true});
+
+    useEffect(() =>{
+        if(isinView){
+            motionValue.set(value);
+        } 
+    }, [isinView, value, motionValue])
+
+    useEffect(()=>{
+        springValue.on("change", (latest)=>{
+            if(ref.current && latest.toFixed(0) <= value){
+                ref.current.textContent = latest.toFixed(0);
+            }
+        })
+    }, [springValue, value])
+
+    return <span ref = {ref}></span>
+}
 
 const about = () => {
     return (
@@ -16,31 +41,43 @@ const about = () => {
         <main className='flex w-full flex-col items-center justify-center'>
             <Layout className='pt-16'>
                 <AnimatedText text = "Passion Fuels Purpose!" className='mb-16'/>
-                <div className='grid w-full grid-cols-8 gap-16'>
-                    <div className='col-span-3 flex flex-col items-start justify-start'>
+                <div className='grid w-full grid-cols-9 gap-16'>
+                    <div className='col-span-4 flex flex-col items-start justify-start'>
                         <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
-                        <p className='font-medium '>Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
-                        and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                        new and innovative ways to bring my clients' visions to life.</p>
+                        <p className='font-medium '>Hello, I’m <b>Adinath Mahangare</b>, a software developer and machine learning enthusiast with a passion for creating innovative, intelligent, and user-friendly digital solutions. I am always eager to learn new skills and technologies to enhance my capabilities.</p>
 
-                        <p className='font-medium my-4'>I believe that design is about more than just making things look pretty – it's about solving problems and creating intuitive, enjoyable experiences for users.</p> 
+                        <p className='font-medium my-4'>I believe that software development is about more than just writing code – it’s about solving problems and creating impactful, meaningful experiences for users. I have worked on various projects involving web development, mobile development, data science, natural language processing, computer vision, and deep learning. I have used a range of tools and frameworks such as Python, C++, Django, Flask, React, NextJS, Firebase, Tailwind, SQL, Git and Linux.</p> 
 
-                        <p className='font-medium '> Whether I'm working on a website, mobile app, or 
-                        other digital product, I bring my commitment to design excellence and user-centered thinking to 
-                        every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
+                        <p className='font-medium '>Whether I’m working on a web app, a mobile app, or a data science project, I bring my dedication to excellence and user-centric thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
                     </div>
                     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
                         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
                         <Image src = {profilePic} alt = "Adinath" className='w-full h-auto rounded-2xl' /> 
                     </div>
-                <div>
-                    <div>
-                        <span>
-                            50+
+                <div className='col-span-2 flex flex-col items-end justify-between'>
+                    <div className='flex flex-col items-end justify-center'>
+                        <span className='inline-block text-7xl font-bold'>
+                            <AnimatedNumbers value={15}/>+
                         </span>
-                        <h2>Satisfied clients</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75'>Projects Completed</h2>
                     </div>
+
+                    <div className='flex flex-col items-end justify-center'>
+                        <span className='inline-block text-7xl font-bold'>
+                            <AnimatedNumbers value={10}/>+
+                        </span>
+                        <h2 className='text-xl font-medium capitalize text-dark/75'>Certifications</h2>
+                    </div>
+                
+                    <div className='flex flex-col items-end justify-center'>
+                        <span className='inline-block text-7xl font-bold'>
+                            <AnimatedNumbers value={1}/>+
+                        </span>
+                        <h2 className='text-xl font-medium capitalize text-dark/75'>Years of Experience</h2>
+                    </div>
+
                 </div>
+
                 </div>
             </Layout>
             
