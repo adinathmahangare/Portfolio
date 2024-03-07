47b10537b090fc5810f75537a6345b62071935a8
Adinath Mahangare <adinathmahangare914@gmail.com>
2024-03-07 16:01:24 +0530

    Add page Animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
 import HireMe from '@/components/HireMe'
 import lightBulb  from '../../public/images/svgs/miscellaneous_icons_1.svg'
+import TransitionEffect from '@/components/TransitionEffect'
 
 
 
@@ -19,6 +20,7 @@ export default function Home() {
         
       </Head>
       
+      <TransitionEffect/>
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
           
           <Layout className='pt-0 md:pt-16 sm:pt-8'>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,9 +4,11 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
-import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
-import project3 from "../../public/images/projects/WFD3.png"
+import project1 from "../../public/images/projects/HCS.png"
+import project3 from "../../public/images/projects/WFD.png"
+import project2 from "../../public/images/projects/FCA.png"
 import Image from 'next/image'
+import TransitionEffect from '@/components/TransitionEffect'
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
     return (
@@ -86,6 +88,7 @@ const projects = () => {
             <meta name = "description" content = "about me" />
         </Head>
 
+        <TransitionEffect/>
         <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'>
                 <AnimatedText text = "Where Ideas Take Flight!" className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl '/>
@@ -93,23 +96,23 @@ const projects = () => {
                 <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
                     <div className='col-span-12'>
                         <FeaturedProject 
-                        title="Crypto Screener Application"
-                        summary = "A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."
+                        title="Smart Healthcare System"
+                        summary = "A web-based system bridges the gap between local clinicians and hospital chains by securely storing and sharing the comprehensive medical history of every patient."
                         link ="/"
                         type = "Featured Project"
                         img = {project1}
-                        github = "/"
+                        github = "https://github.com/adinathmahangare/Final_Year_Project"
                         />
 
                     </div>
                     <div className='col-span-6 sm:col-span-12'>
                         <Project 
-                        title="Crypto Screener Application"
+                        title="Firebase Contact App"
                         summary = "A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."
-                        link ="/"
-                        type = "Featured Project"
-                        img = {project1}
-                        github = "/"
+                        link ="https://contact-app-three-iota.vercel.app/"
+                        type = "Web Application"
+                        img = {project2}
+                        github = "https://github.com/adinathmahangare/contact-app"
                         />
 
                     </div>
